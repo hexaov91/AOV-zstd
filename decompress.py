c04d3fbf4beeaaae93e3f1dd8dfd828c67b7e7e0
hexaov91 <60669781+hexaov91@users.noreply.github.com>
2022-09-28 22:01:41 +0800

    Update decompress.py

--- decompress.py
+++ decompress.py
@@ -8,19 +8,27 @@ import pathlib
 import zstd
 import zstandard
 import time
+
+import codecs
+
+
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import concurrent.futures
+
 success= 0
 fail =0
+
 deleatlist=[]
 os.chdir(os.getcwd())
 print(os.getcwd())
 
-InputPath='input'
-OutputPath='output'
-dictPath=r'dict'
-with open(dictPath, 'rb') as f:
+
+with open(r'DS', 'rb') as f:
 	dict_data = f.read()
 dict_data = ZstdCompressionDict(dict_data)
 del f
+
 ZSTD_DECOMPRESS_INSTANT = ZstdDecompressor
 ZSTD_COMPRESS_INSTANT = ZstdCompressor
 Zstd_METHOD_MAPPING = {239: ((ZstdDecompressor, {'dict_data': dict_data}), (ZstdCompressor, {'dict_data': dict_data})), 103: ((None, None), (None, None))}
@@ -38,32 +46,18 @@ def makedir(path):
 		os.makedirs(path)
 
 
-def get_methods(data, mode=False):
-	methods = []
-	if data[:2] != b'"J':
-		return
-	for i in data[2:]:
-		if i:
-			methods.append(Zstd_METHOD_MAPPING[i][mode])
-	return methods
+
+def de_compress_file(data, len):
+
+	dctx = zstandard.ZstdDecompressor(dict_data=dict_data)
+	decompressed = dctx.decompress(data)
+	return decompressed
 
 
-def de_compress_file(f, mode=False):
-	
-	methods = get_methods(f.read(4), mode)
-	lens = int.from_bytes(f.read(4), byteorder='little')
-	data = f.read(lens)
-	for func, argus in methods:
-		if func is None:
-			raise ('error:')
-		instant = func(**argus)
-		data = instant.decompress(data)
-	return data
-
-
+d1=[]
+d2=[]
 def de_compress_folder(inp, out, mode=False):
-	global success
-	global fail
+
 	inp = modifypath(inp, os.path.sep)
 	out = modifypath(out, os.path.sep)
 	for path, _, file_names in os.walk(inp):
@@ -71,43 +65,87 @@ def de_compress_folder(inp, out, mode=False):
 		makedir(output_path)
 		for file_name in file_names:
 			input_path = path + os.path.sep + file_name
-			with open(input_path, 'rb') as f:
-				with open(output_path + os.path.sep + file_name, 'wb') as of:
-					try:
-						of.write(de_compress_file(f, mode))
-						success +=1
-					except TypeError as e:
-						deleatlist.append(output_path + os.path.sep + file_name)
-						fail +=1
+			output_path=out + path[len(inp):] + os.path.sep + file_name
+			d1.append(input_path)
+			d2.append(output_path)
+
+	with concurrent.futures.ThreadPoolExecutor(max_workers=128) as executor:
+	  executor.map(third, d1,d2)
+
+def third(input_path, output_path):
+	global success
+	global fail
+	with open(input_path, 'rb') as f:
+		tmp=f.read()
+		header=tmp[0:4]
+		lens=tmp[4:8]
+		data=tmp[8:]
+		with open(output_path, 'wb') as of:
+			if header == b'\x22\x4a\x00\xef':
+				try:
+					of.write(de_compress_file(data, lens))
+					success +=1
+				except TypeError as e:
+					deleatlist.append(output_path)
+					fail +=1
+
+			elif header == b'\x22\x4a\x67\x00':
+				try:
+					#of.write(decrypt_file(input_path,data))
+					success +=1
+				except TypeError as e:
+					deleatlist.append(output_path)
+					fail +=1
+
+			else:
+				print("未知類型",input_path,header)
+
+
+
 
 def unpackpkg(input):
- pkgpath=input
- for root, dirs, files in walk(pkgpath):
-  for f in files:
-    fullpath = join(root, f)
-    pkg = pathlib.Path(fullpath)
-    if ''.join(pkg.suffixes) == '.pkg.bytes' :
+	u1=[]
+	u2=[]
+	pkgpath=input
+	for root, dirs, files in walk(pkgpath):
+		for f in files:
+			fullpath = join(root, f)
+			pkg = pathlib.Path(fullpath)
+			if ''.join(pkg.suffixes) == '.pkg.bytes' :
+				u2.append(fullpath)
+			else:
+				pass
+	with concurrent.futures.ThreadPoolExecutor(max_workers=128) as executor:
+		executor.map(unpackpkgThird,u2)
+	print("unpack End")
+
+def unpackpkgThird(fullpath):
      with zipfile.ZipFile(fullpath, 'r') as zip_ref:
       zip_ref.extractall(os.path.dirname(fullpath))
       zip_ref.close()
-      os.remove(fullpath)				
-				
-input=InputPath
-output=OutputPath
+      os.remove(fullpath)	
+      
+
+
+
+input='待工作區'
+output='Output'
 
 start = time.time()
 unpackpkg(input)
 de_compress_folder(input,output)
+#os.remove('error.log')
+#print(deleatlist)
 
-for delfile in deleatlist:
- os.remove(delfile)
- with open(delfile + 'error' , 'w') as of:
-   of.write('error')
- with open(r'error.log', 'a') as f:
-    f.writelines(delfile+'\n')
+ 
+
+
+
    
 end = time.time()   
 print("解壓了"+ str(success) + f"個文件 於 {end - start} 秒,真的太星爆了")
+
+
    
-   
+
    
